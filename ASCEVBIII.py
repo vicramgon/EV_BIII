4a1d19d3ce2f7f5439635231ef7e2a98d364d3b4
Victor Ramos <vicramgon.us@gmail.com>
2021-03-20 13:58:18 +0100

    parcial v1

--- ASCEVBIII.py
+++ ASCEVBIII.py
@@ -1,95 +1,104 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 15 19:12:22 2021
-
-@author: yhatctor
-"""
 
 """ 
 ==============================================================================
                APLICACIONES DE SOFT-COMPUTING
 ==============================================================================
 
-
+@title : COMPETICIÓN B.III - Algoritmos Evolutivos : MOEA/D
+@author: Víctor Ramos González
 """
+'''
+##############
+# LIBRARIES #
+#############
+'''
 
 import numpy as np                 # Math library
-import matplotlib.pyplot as plt    # Plot library
 import os                          # Create directories
 import shutil                      # Delete directories
-import colour
 
-def EOP1(cur, P, VP, Bcur, bounds, F=0.5, CR=0.5, SIG=20, pm=None, seed=None):
-    np.random.seed(seed)
+
+'''
+#######################
+# EOP IMPLEMENTATION #
+######################
+'''
+
+def EOP1(i, P, VP, Bi, searchSpace, F=0.5, CR=0.5, SIG=20, pm=None, seed=None):
     # 1) DIFFERENTIAL EVOLUTION MUTATION
     p = P.shape[1]
     PM = 1/p if pm is None else pm
-    r = P[np.random.choice(Bcur, 3, replace=False)]
+    r = P[np.random.choice(Bi, 3, replace=False)]
     yhat = r[0] + F*(r[1]-r[2])
     
     # 2) DIFFERENTIAL EVOLUTION CROSSING
     delta = np.random.randint(0,p)
-    M = np.array([True if np.random.random()<=CR else False for i in range(p)])
+    M = np.array([True if np.random.random()<=CR else False for _ in range(p)])
     M[delta] = True 
     
-    y = M.astype(np.float64)*yhat + (~M).astype(np.float64)*P[cur]
+    y = M.astype(np.float64)*yhat + (~M).astype(np.float64)*P[i]
     
-    # EOP1.3) GAUSSIAN MUTATION AND REPARING
+    # 3) GAUSSIAN MUTATION AND REPARING
     for j, yj in enumerate(y):
         if np.random.random() < PM:
-            y[j] = min(max(yj+ np.random.normal(0,(bounds[j][1]-bounds[j][0])/SIG),bounds[j][0]),bounds[j][1])
+            y[j] = min(max(yj+ np.random.normal(0,(searchSpace[j][1]-searchSpace[j][0])/SIG),searchSpace[j][0]),searchSpace[j][1])
         else:
-            y[j] = min(max(yj,bounds[j][0]),bounds[j][1])
+            y[j] = min(max(yj,searchSpace[j][0]),searchSpace[j][1])
     return y
 
-def EOP2(cur, P, VP, Bcur, bounds, seed=None, F=0.5, CR=0.5, pm=None, ETA=10):
-    np.random.seed(seed)
+def EOP2(i, P, VP, Bi, searchSpace, seed=None, F=0.5, CR=0.5, pm=None, ETA=10):
     # 1) DIFFERENTIAL EVOLUTION MUTATION
     p = P.shape[1]
     PM = 1/p if pm is None else pm
-    r = P[np.random.choice(Bcur, 5, replace=False)]
+    r = P[np.random.choice(Bi, 5, replace=False)]
     yhat = r[0] + F*(r[1]-r[2]) + np.random.random()*(r[3]-r[4])
     
     # 2) DIFFERENTIAL EVOLUTION CROSSING
     delta = np.random.randint(0,p)
-    M = np.array([True if np.random.random()<=CR else False for i in range(p)])
+    M = np.array([True if np.random.random()<=CR else False for _ in range(p)])
     M[delta] = True 
     
-    y = M.astype(np.float64)*yhat + (~M).astype(np.float64)*P[cur]
+    y = M.astype(np.float64)*yhat + (~M).astype(np.float64)*P[i]
     
-    # SBX MUTATION 
+    # 3) SBX MUTATION 
     for j, yj in enumerate(y):
         if np.random.random() < PM :
             mu =  np.random.random()
             sigmaj = (2*mu)**(1/(ETA+1)) if mu < 0.5 else 1 - (2 - 2*mu)**(1/(ETA+1))
-            y[j] += sigmaj*(bounds[j][1] - bounds[j][0])
+            y[j] += sigmaj*(searchSpace[j][1] - searchSpace[j][0])
         
-        if y[j] < bounds[j][0] or y[j] > bounds[j][1]:
-            y[j] = min(max(yj,bounds[j][0]),bounds[j][1])
+        # REPARING
+        if y[j] < searchSpace[j][0] or y[j] > searchSpace[j][1]:
+            y[j] = min(max(yj,searchSpace[j][0]),searchSpace[j][1])
     return y
 
 
-def EOP3(cur, P, VP, Bcur, bounds, seed=None, e=2, CR=1, SIG=20, pm=None):
-    np.random.seed(seed)
-    # 1) DIFFERENTIAL EVOLUTION MUTATION
-    
+def EOP3(i, P, VP, Bi, searchSpace, seed=None, e=4, SIG=20, pm=None):
     p = P.shape[1]
     PM = 1/p if pm is None else pm
-    r = P[np.random.choice(Bcur, e, replace=False)]
-    y = np.array([ r[np.random.randint(0,e)][j] for j in range(p)])
+    
+    # 1) UX CROSSOVER
+    r = P[np.random.choice(Bi, e, replace=False)]
+    y = np.array([r[np.random.randint(0,e)][j] for j in range(p)])
     
     
     # EOP1.3) GAUSSIAN MUTATION 
     for j, yj in enumerate(y):
         if np.random.random() < PM:
-            y[j] = min(max(yj+ np.random.normal(0,(bounds[j][1]-bounds[j][0])/SIG),bounds[j][0]),bounds[j][1])
+            y[j] = min(max(yj+ np.random.normal(0,(searchSpace[j][1]-searchSpace[j][0])/SIG),searchSpace[j][0]),searchSpace[j][1])
         else:
-            y[j] = min(max(yj,bounds[j][0]),bounds[j][1])
+            y[j] = min(max(yj,searchSpace[j][0]),searchSpace[j][1])
     return y
     
 
-def ASCEVBIII(Fgoals, vars_limits, N, G, T, eop=EOP1, seed=None, lambdaInput={'path':'./input/lambdas.csv', 'delimiter':'j'}, outputDirPath='./results'):
+'''
+##########################
+# MOEA/D IMPLEMENTATION #
+#########################
+'''
+def MOEAD(goals, searchSpace, N, G, T, eop=EOP3, seed=None, lambdaInput={'path':'./input/lambdas.csv', 'delimiter':'j'}, outputDirPath='./results'):
 
     
     ## SETTINGS & PARAMETERS
@@ -97,9 +106,9 @@ def ASCEVBIII(Fgoals, vars_limits, N, G, T, eop=EOP1, seed=None, lambdaInput={'p
         shutil.rmtree(outputDirPath)
     os.mkdir(outputDirPath) 
     
-    bounds = [sorted(vl) for vl in vars_limits]
-    p=len(bounds)
-    m=len(Fgoals)
+    searchSpace = [sorted(vl) for vl in searchSpace]
+    p=len(searchSpace)
+    m=len(goals)
     np.random.seed(seed)
     
     # INITIALIZATION
@@ -136,21 +145,21 @@ def ASCEVBIII(Fgoals, vars_limits, N, G, T, eop=EOP1, seed=None, lambdaInput={'p
     del D
     
     # We generate a random population in which each component of each individual
-    # is between the corresponding bounds.
+    # is between the corresponding searchSpace.
     P = np.random.random([N, p]).astype(np.float64)
     #print(P);
     
-    for i, bi in enumerate(bounds):
+    for i, bi in enumerate(searchSpace):
         P[:,i] = list(map(lambda x: bi[0] + (bi[1] - bi[0])*x, P[:,i]))
     del i
     
     
-    VP = np.array([np.apply_along_axis(fi, 1, P) for fi in Fgoals]).transpose()
+    VP = np.array([np.apply_along_axis(fi, 1, P) for fi in goals]).transpose()
     
     ## We initialize z vector with the best value of f1 and f2 in the population.
     z = np.apply_along_axis(np.min, 0, VP)
     
-    with open(f"{outputDirPath}/gen0.out", "ab") as resFile:
+    with open(f"{outputDirPath}/{'' if seed is None else 's' + str(seed) + '_'}gen0.out", "ab") as resFile:
         writeVP = np.zeros([N,m+1])
         writeVP[:,:-1] = VP
         np.savetxt(resFile,writeVP, delimiter="\t", newline='\n', header='', footer='')
@@ -160,8 +169,8 @@ def ASCEVBIII(Fgoals, vars_limits, N, G, T, eop=EOP1, seed=None, lambdaInput={'p
         np.random.shuffle(order)
         for cur in order:
             # GENERATE CHILD
-            y = eop(cur, P, VP, B[cur], bounds, seed=seed)
-            vy =  np.array([fi(y) for fi in Fgoals])
+            y = eop(cur, P, VP, B[cur], searchSpace, seed=seed)
+            vy =  np.array([fi(y) for fi in goals])
             
             # UPDATE Z
             z = np.minimum(z, vy)
@@ -173,67 +182,7 @@ def ASCEVBIII(Fgoals, vars_limits, N, G, T, eop=EOP1, seed=None, lambdaInput={'p
                     P[j] = y
                     VP[j] = vy
         
-        with open(f"{outputDirPath}/gen{it+1}.out", "ab") as resFile:
+        with open(f"{outputDirPath}/{'' if seed is None else 's' + str(seed) + '_'}gen{it+1}.out", "ab") as resFile:
             writeVP = np.zeros([N,m+1])
             writeVP[:,:-1] = VP
             np.savetxt(resFile,writeVP, delimiter="\t", newline='\n', header='', footer='')
-
-"""
-###############
-# PARAMETERS #
-##############
-"""
-N = 100
-G = 100
-T = 5
-
-f1 = lambda x : x[0]
-
-def f2(x):
-    g = 1 + 9/(len(x)-1) *sum(x[1:])
-    return g*(1 - np.sqrt(x[0]/g)-(x[0]/g)*np.sin(10*np.pi*x[0]))
-
-vars_limits = [(0,1) for _ in range(30)]
-
-
-
-"""
-##############################
-#   REAL ZDT3 PARETO FRONT  #
-#############################
-"""
-ZDT3_pareto_front_x = np.concatenate((np.linspace(0, 0.0830015349), 
-                        np.linspace(0.1822287281, 0.2577623634),
-                        np.linspace(0.4093136749,0.45388221041),
-                        np.linspace(0.6183967945,0.6525117038),
-                        np.linspace(0.8233317984, 0.8518328654))
-                      )
-
-ZDT3_pareto_front_y = np.vectorize(lambda x: 1 - np.sqrt(x) - x*np.sin(10*np.pi*x))(ZDT3_pareto_front_x)
-
-"""
-##############################
-# ONE EXECUTION DEVELOPMENT #
-#############################
-"""
-
-outputDirPath = './results'
-
-# Executes algorithm one time. Results are saved in ./results directory
-ASCEVBIII([f1, f2], vars_limits, N, G, T,  outputDirPath=outputDirPath);
-
-
-colors = list(colour.Color("blue").range_to(colour.Color("red"),G))
-# Plot an execution development 
-for generation in range(G):
-    my_data = np.genfromtxt(f"{outputDirPath}/gen{generation}.out", delimiter='\t')
-    plt.scatter(my_data[:,0], my_data[:,1], s=1, c=[colors[generation].rgb for _ in range(N)], cmap='gray')
-    
-#  Plot real front
-plt.scatter(ZDT3_pareto_front_x, ZDT3_pareto_front_y, s=1, c='black', alpha=0.3)
-
-
-
-
-
-
---
+++ PROOFS_ZDT3_EOP1.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" 
+==============================================================================
+               APLICACIONES DE SOFT-COMPUTING
+==============================================================================
+
+@title : COMPETICIÓN B.III - Algoritmos Evolutivos : Pruebas
+@author: Víctor Ramos González
+"""
+
+'''
+##############
+# LIBRARIES #
+#############
+'''
+from ASCEVBIII import EOP1, MOEAD
+import numpy as np                 # Math library
+import matplotlib.pyplot as plt    # Plot library
+import colour                      # Color library
+
+'''
+###############################################
+# ZDT3: GOALS, SEARCH SPACE AND PARETO FRONT #
+##############################################
+'''
+
+# GOALS FUNCTIONS
+f1 = lambda x : x[0]
+
+def f2(x):
+    g = 1 + 9/(len(x)-1) *sum(x[1:])
+    return g*(1 - np.sqrt(x[0]/g)-(x[0]/g)*np.sin(10*np.pi*x[0]))
+
+ZDT3_GOALS = [f1, f2]
+
+# SEARCH SPACE
+ZDT3_SS = [(0,1) for _ in range(30)]
+
+# REAL PARETO FRONT
+ZDT3_PF_x = np.concatenate((np.linspace(0, 0.0830015349), 
+                        np.linspace(0.1822287281, 0.2577623634),
+                        np.linspace(0.4093136749,0.45388221041),
+                        np.linspace(0.6183967945,0.6525117038),
+                        np.linspace(0.8233317984, 0.8518328654))
+                      )
+
+ZDT3_PF_y = np.vectorize(lambda x: 1 - np.sqrt(x) - x*np.sin(10*np.pi*x))(ZDT3_PF_x)
+
+#%%
+'''
+#############################
+# PROOF 1.1: MOEA/D + EOP1 #
+#############################
+'''
+
+# MOEA PARAMETERS
+N = 100; G = 100; T=10; eop=EOP1
+
+# FILES 
+outputDirPath = f'./results_ZDT3_{eop.__name__}_N{N}_G{G}_T{T}'
+
+# EXECUTION ALGORITHM. 
+# Results are saved in given directory with name:
+#   "gen{nº gen}.out"         if no seed is given
+#   "s{seed}_gen{nº gen}.out" in other case
+MOEAD(ZDT3_GOALS, ZDT3_SS, N, G, T, eop=EOP1, outputDirPath=outputDirPath);
+
+# RESULTS PLOTS
+colors = list(colour.Color("blue").range_to(colour.Color("red"),G))
+
+# Plot an execution development 
+for generation in range(G):
+    my_data = np.genfromtxt(f"{outputDirPath}/gen{generation}.out", delimiter='\t')
+    plt.scatter(my_data[:,0], my_data[:,1], s=1, c=[colors[generation].rgb for _ in range(N)], cmap='gray')
+    
+#  Plot real front
+plt.scatter(ZDT3_PF_x, ZDT3_PF_y, s=1, c='black', alpha=0.2)
+
+#  Plot title, axis labels and grid
+plt.title(f"Development ZDT3_{eop.__name__}_N{N}_G{G}_T{T}")
+plt.xlabel('$f_1$')
+plt.ylabel('$f_2$')
+plt.grid()
+
+
+plt.savefig(f"./plotResults/ZDT3_{eop.__name__}_N{N}_G{G}_T{T}_development.png")
+plt.show()
+
+#%%
+'''
+#############################
+# PROOF 1.2: MOEA/D + EOP1 #
+############################
+'''
+
+# MOEA PARAMETERS
+N = 50; G = 200; T=4; eop=EOP1
+
+# FILES 
+outputDirPath = f'./results_ZDT3_{eop.__name__}_N{N}_G{G}_T{T}'
+
+# EXECUTION ALGORITHM. 
+# Results are saved in given directory with name:
+#   "gen{nº gen}.out"         if no seed is given
+#   "s{seed}_gen{nº gen}.out" in other case
+MOEAD(ZDT3_GOALS, ZDT3_SS, N, G, T, eop=EOP1, outputDirPath=outputDirPath);
+
+# RESULTS PLOTS
+colors = list(colour.Color("blue").range_to(colour.Color("red"),G))
+
+# Plot an execution development 
+for generation in range(G):
+    my_data = np.genfromtxt(f"{outputDirPath}/gen{generation}.out", delimiter='\t')
+    plt.scatter(my_data[:,0], my_data[:,1], s=1, c=[colors[generation].rgb for _ in range(N)], cmap='gray')
+    
+#  Plot real front
+plt.scatter(ZDT3_PF_x, ZDT3_PF_y, s=1, c='black', alpha=0.2)
+
+#  Plot title, axis labels and grid
+plt.title(f"Development ZDT3_{eop.__name__}_N{N}_G{G}_T{T}")
+plt.xlabel('$f_1$')
+plt.ylabel('$f_2$')
+plt.grid()
+
+
+plt.savefig(f"./plotResults/ZDT3_{eop.__name__}_N{N}_G{G}_T{T}_development.png")
+plt.show()
+
+#%%
+'''
+#############################
+# PROOF 1.3: MOEA/D + EOP1 #
+#############################
+'''
+
+# MOEA PARAMETERS
+N = 200; G = 50; T = 15; eop=EOP1
+
+# FILES 
+outputDirPath = f'./results_ZDT3_{eop.__name__}_N{N}_G{G}_T{T}'
+
+# EXECUTION ALGORITHM. 
+# Results are saved in given directory with name:
+#   "gen{nº gen}.out"         if no seed is given
+#   "s{seed}_gen{nº gen}.out" in other case
+MOEAD(ZDT3_GOALS, ZDT3_SS, N, G, T, eop=EOP1, outputDirPath=outputDirPath);
+
+# RESULTS PLOTS
+colors = list(colour.Color("blue").range_to(colour.Color("red"),G))
+
+# Plot an execution development 
+for generation in range(G):
+    my_data = np.genfromtxt(f"{outputDirPath}/gen{generation}.out", delimiter='\t')
+    plt.scatter(my_data[:,0], my_data[:,1], s=1, c=[colors[generation].rgb for _ in range(N)], cmap='gray')
+    
+#  Plot real front
+plt.scatter(ZDT3_PF_x, ZDT3_PF_y, s=1, c='black', alpha=0.2)
+
+#  Plot title, axis labels and grid
+plt.title(f"Development ZDT3_{eop.__name__}_N{N}_G{G}_T{T}")
+plt.xlabel('$f_1$')
+plt.ylabel('$f_2$')
+plt.grid()
+
+
+plt.savefig(f"./plotResults/ZDT3_{eop.__name__}_N{N}_G{G}_T{T}_development.png")
+plt.show()
+
